351b47d85b882859585cb8bced99c264dfdcfb8d
evgenytoropov <evgenytoropov@deepmap.ai>
2020-05-01 09:15:30 -0400

    Added file dbStamps with subcommands upgradeStampImagepaths and extractNumberIntoProperty

--- lib/subcommands/__init__.py
+++ lib/subcommands/__init__.py
@@ -1,5 +1,6 @@
 from lib.subcommands import dbGui, dbInfo, dbFilter, dbModify, dbMedia
 from lib.subcommands import dbEvaluate, dbLabel, dbMatplotlib
+from lib.subcommands import dbStamps
 from lib.subcommands.datasets import dbLabelme, dbKitti, dbPascal, dbBdd
 from lib.subcommands.datasets import dbDetrac, dbCityscapes, dbCoco
 
@@ -23,3 +24,4 @@ def add_subparsers(parser):
     dbCityscapes.add_parsers(subparsers)
     dbCoco.add_parsers(subparsers)
     dbMatplotlib.add_parsers(subparsers)
+    dbStamps.add_parsers(subparsers)
