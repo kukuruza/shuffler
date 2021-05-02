f4d0bbe244158ad55193b72524443fc53860ca95
kukuruza <toropov.evgeny@gmail.com>
2021-05-02 17:18:30 -0400

    Dataframe plots are changed once plotted

--- interface/dataframe_demo.py
+++ interface/dataframe_demo.py
@@ -13,8 +13,16 @@ df.load('testdata/cars/micro1_v4.db', rootdir='testdata/cars')
 df.printInfo()
 df.load(tmp_file_path, rootdir='testdata/cars')
 df.printInfo()
+# Create a figure before calling imaging function in order to use Figure object.
+fig = plt.figure(figsize=(5, 15))
 df.displayImagesPlt(limit=4, with_objects=True, with_imagefile=True)
 plt.show()
+
+# Create a figure before calling imaging function in order to use Figure object.
+fig = plt.figure(figsize=(15, 5))
+df.plotHistogram(sql='SELECT width FROM objects')
+plt.show()
+
 print(len(df))
 image, objects = df[1]['image'], df[1]['objects']
 plt.imshow(image)
